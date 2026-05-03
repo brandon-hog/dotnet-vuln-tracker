6c91000a23471173bd13655bb948935f1aa44e53
brandon-UWF <bmh86@students.uwf.edu>
2026-05-03 16:00:40 -0500

    feat: adding docker configuration for running the application!

--- Api/Program.cs
+++ Api/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowBlazorClient", policy =>
     {
         // Blazor WASM local port
-        policy.WithOrigins(allowedOrigins ?? ["localhost"]) 
+        policy.WithOrigins(allowedOrigins ?? ["http://localhost"]) 
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
@@ -60,6 +60,14 @@ builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+// Initialize the database if not initialized, and updates the schema without deleting data
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    // Applies pending migrations or creates the database if it doesn't exist
+    dbContext.Database.Migrate(); 
+}
 
 // Middleware definition
 app.UseSwagger();
